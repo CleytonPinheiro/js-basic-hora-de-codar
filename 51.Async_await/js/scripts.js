6120777b0c95dff929141755627ff55895430377
Cleyton <42612444+CleytonPinheiro@users.noreply.github.com>
2022-12-18 13:28:50 -0300

    async await

---
+++ 51.Async_await/js/scripts.js
@@ -0,0 +1,39 @@
+//Sintaxe
+function primeiraFuncao() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('Esperou isso aqui');
+      resolve();
+    }, 1000)
+  })
+};
+
+async function segundaFuncao() {
+  console.log('Iniciou');
+
+  await primeiraFuncao();
+
+  console.log('Terminou');
+}
+
+segundaFuncao();
+
+//Prática
+function getUser(id) {
+  return fetch(`https://reqres.in/api/users?id=${id}`)
+    .then(data => data.json())
+    .catch(err => console.log(err));
+};
+
+async function showUserName(id) {
+
+  try {
+    const user = await getUser(id);
+
+    console.log(`O nome do usuário é: ${user.data.first_name}`);
+  } catch (error) {
+     console.log(`Erro: ${error}`);
+  } 
+};
+
+showUserName(2);
